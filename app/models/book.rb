59b5c2cbafabd7334134c3042bf20b80cb3dfeee
lionzxy <nikita@kulikof.ru>
2016-03-01 23:27:38 +0300

    Search alogorithm

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,8 +1,8 @@
 class Book < ActiveRecord::Base
-
   #after_initialize {
   #TODO optimize books
   #}
+
 
   def getSearchIndex(search_req)
     search_eq = self.search_index.to_i
@@ -11,20 +11,63 @@ class Book < ActiveRecord::Base
   end
 
   def eq_text(find_w, source)
-    arr_f = find_w.to_s.split(' ')
-    arr_s = source.to_s.split(' ')
-    indexs = []
-    arr_f.each do |word|
-      indexs_tmp = []
-      arr_s.each { |word_s| indexs_tmp.push eq_word word, word_s }
-      indexs.push indexs_tmp
-    end
     #TODO rewrite shitcode
+    words_arr = find_w.to_s.split ' '
+    words_s_arr = source.to_s.split ' '
+    n = words_arr.length
+    m = words_s_arr.length
+
+    return m if (0 == n)
+    return n if (0 == m)
+
+    d = (0..m).to_a
+    x = nil
+
+    words_arr.each_with_index do |word1, i|
+      e = i + 1
+
+      words_s_arr.each_with_index do |word2, j|
+        x = [
+            #d[j + 1] +
+        ]
+      end
+
+    end
+    distance("Something", "Smoething")
   end
 
-  def eq_word(word, word_source)
-    #TODO calculated word eq
+  def self.distance str1, str2
+    #This code from https://github.com/GlobalNamesArchitecture/damerau-levenshtein
+    n = str1.length
+    m = str2.length
+
+    return m if (0 == n)
+    return n if (0 == m)
+
+    #Храним лишь одномерный массив.
+    d = (0..m).to_a
+    x = nil
+
+    str1.each_char.each_with_index do |char1, i|
+      e = i+1
+
+      str2.each_char.each_with_index do |char2, j|
+        cost = (char1 == char2) ? 0 : 1
+        x = [
+            d[j+1] + 1, # insertion
+            e + 1, # deletion
+            d[j] + cost # substitution
+        ].min
+        d[j] = e
+        e = x
+      end
+
+      d[m] = x
+    end
+
+    return x
   end
+
 
   validates :name, presence: true
   validates :author, presence: true
@@ -42,19 +85,25 @@ class Book < ActiveRecord::Base
   def getAuthor
     if self.author.nil?
       'Неизвестный автор'
-    else self.author end
+    else
+      self.author
+    end
   end
 
   def getAnnotation
     if self.annotation.nil?
       'Аннотация отсутсвует'
-    else self.annotation end
+    else
+      self.annotation
+    end
   end
 
   def getName
     if self.name.nil?
       'Неизвестная книга'
-    else self.name end
+    else
+      self.name
+    end
   end
 
 end
