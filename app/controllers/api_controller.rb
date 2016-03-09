a054f8f1ac2279a627374bb25f675bc9247a6544
lionzxy <nikita@kulikof.ru>
2016-03-09 16:10:49 +0300

    Add version control

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -2,6 +2,10 @@ class ApiController < ApplicationController
   require 'digest/md5'
   #Digest::MD5.hexdigest("Hello from Dmitry")
 
+  def self.version
+    return '0.1c ALPHA'
+  end
+
   def register
 
     #render text: '1'
--- app/models/book.rb
+++ app/models/book.rb
@@ -9,9 +9,9 @@ class Book < ActiveRecord::Base
 
   def getSearchIndex(search_req)
     search_eq = self.search_index.to_i
-    search_eq += Search.new.eq_text search_req, getName
-    search_eq += Search.new.eq_text search_req, getAuthor
-    search_eq += (Search.new.eq_text search_req, getAnnotation) / 2
+    search_eq += (Search.new.eq_text(search_req, getName)/getName.length) * 100
+    search_eq += ((Search.new.eq_text search_req, getAuthor)/getAuthor.length)*100
+    search_eq += ((Search.new.eq_text search_req, getAnnotation)/getAnnotation.length)*50
 
     return search_eq
   end
